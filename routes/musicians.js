3a79f660c6d48fa88342550ba8d12bf54d99d9b0
kblack-git <keithblack@gmail.com>
2023-04-14 14:22:15 -0400

    created musicians route

--- routes/musicians.js
+++ routes/musicians.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const fs = require ('fs')
-const BANDS_FILE = './data/bands.json'
+const MUSICIANS_FILE = './data/musicians.json'
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  fs.readFile(BANDS_FILE, 'utf8',(err,data) => {
+  fs.readFile(MUSICIANS_FILE, 'utf8',(err,data) => {
       if (err) {
           console.error(err);
           res.status(500).send('There was a problem reading the file...')
@@ -17,40 +17,40 @@ router.get('/', function(req, res, next) {
 
 router.get('/:id', (req, res, next)  => {
 
-    fs.readFile(BANDS_FILE, 'utf8', (err,data) => {
+    fs.readFile(MUSICIANS_FILE, 'utf8', (err,data) => {
         if (err) {
             console.error(err);
             res.status(500).send('There was a problem reading the file...')
             return;    
         }
-        const bands = JSON.parse(data);
-        const band = bands.find(band => band.id === req.params.id)
+        const musicians = JSON.parse(data);
+        const musician = musicians.find(musician => musician.id === req.params.id)
             console.log(band)
             
-            if(!band) {
+            if(!musician) {
             res.status(404).send('Band not found')
             return;
         }   
-        res.json(band) 
+        res.json(musician) 
     })
 })
 
 //POST a new band
 router.post('/', (req,res) => {
-    fs.readFile(BANDS_FILE, 'utf8', (err,data) => {
+    fs.readFile(MUSICIANS_FILE, 'utf8', (err,data) => {
         if (err) {
             console.error(err);
             res.status(500).send('There was a problem reading the file...')
             return;    
         }
         // create new var with data from the request and convert from JSON to regular JS object
-        const bands = JSON.parse(data);
+        const musicians = JSON.parse(data);
         // res.send(bands)
-        const newBand = {
-            id:(bands.length +1).toString(),
+        const newMusician = {
+            id:(musicians.length +1).toString(),
             name: req.body.name,
             active: req.body.active,
-            yearFormed: req.body.yearFormed,
+            band: req.body.band,
             photo_url: req.body.photo_url}
         bands.push(newBand)
         fs.writeFile(BANDS_FILE, JSON.stringify(bands), err => {
@@ -59,7 +59,7 @@ router.post('/', (req,res) => {
                 res.status(500).send('There was a problem writing to the file')
                 return;
             }
-            res.send(newBand)
+            res.send(newMusician)
         })
 
     })
